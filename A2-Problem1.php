7e275b7b4a372f8b0f2698a1229c2672415aaab1
LSHJack <jacklam.1128@gmail.com>
2021-10-26 09:24:59 +1300

    Class Activity 2 - Jack

---
+++ A2-Problem1.php
@@ -0,0 +1,60 @@
+<?php
+
+    class Cat
+    {
+        private $name;
+        private $age;
+        private $breed;
+
+        public function __construct()
+        {
+        }
+    
+        public function set_name($name)
+        {
+            $this->name = $name;
+        }
+
+        public function set_age($age)
+        {
+            $this->age = $age;
+        }
+
+        public function set_breed($breed)
+        {
+            $this->breed = $breed;
+        }
+
+        public function get_name()
+        {
+            return $this->name;
+        }
+
+        public function get_age()
+        {
+            return $this->age;
+        }
+
+        public function get_breed()
+        {
+            return $this->breed;
+        }
+
+        public function __toString()
+        {
+            return "My " . $this->breed . " ’s name is ". $this->name. ". S/he is " . $this->age . " year(s) old.";
+        }
+    }
+
+    $cat_one = new Cat();
+    $cat_one->set_name("Fido");
+    $cat_one->set_age(10);
+
+    $cat_two = new Cat();
+    $cat_two->set_breed("American Bobtail");
+
+    echo $cat_one;
+    echo nl2br("\n");
+    echo $cat_two;
+
+?>
---
+++ A2-Problem4.php
@@ -0,0 +1,83 @@
+<?php
+
+    class Stack
+    {
+        private $stack;
+        public function __construct()
+        { 
+            $this->stack = array();
+        }
+        public function push($item)
+        { 
+            $this->stack[count($this->stack)]=$item;
+        }
+
+        public function pop()
+        {
+            $delete_item=array_splice($this->stack,count($this->stack)-1,1);
+            return $delete_item[0];
+        }
+
+        public function peek()
+        {
+            //Unknown Function
+        }
+
+        public function is_empty()
+        { 
+            if(count($this->stack)==0) return true;
+            return false;
+        }
+
+        public function size()
+        {
+            return count($this->stack);
+        }
+
+        public function show_all()
+        { 
+            echo nl2br("--Show All Item(s)--\n");
+            for($i=count($this->stack)-1;$i>=0;$i--) echo $this->stack[$i]. nl2br("\n");
+        }
+
+        public function __toString()
+        {
+            //Error
+        }
+    }
+
+    $stack = new Stack();
+
+    //Check If Stack Is Empty"
+    if($stack->is_empty()) echo nl2br("Item is empty.\n");
+    else n2br("Item is not empty.\n\n");
+
+    //Add 2 Items
+    echo nl2br("\n --Show All After Added Two Items |   Top Is The Latest Pushed Item--\n\n");
+    $stack->push("Introductory App Dev Concepts");
+    $stack->push("Intermediate App Dev Concepts");
+
+    $stack->show_all();
+    echo "There are ". $stack->size() . nl2br(" item(s) in the stack.\n");
+
+    //Add 1 Item
+    echo nl2br("\n --Show All After Added 1 Item    |   Top Is The Latest Pushed Item--\n\n");
+    $stack->push("Advanced App Dev Concepts");
+    $stack->show_all();
+    echo "There are ". $stack->size() . nl2br(" item(s) in the stack.\n");
+
+    //Pop test
+    echo nl2br("\n --Pop Test   |   1 Item Is Deleted By LIFO--\n");
+    $delete_item=$stack->pop();
+    echo nl2br("$delete_item is deleted.\n\n");
+
+    //Show All
+    $stack->show_all();
+    echo "There are ". $stack->size() . nl2br(" item(s) in the stack.\n");
+
+    //Expected output:
+    //["Introductory App Dev Concepts", "Intermediate App Dev Concepts"]
+    //Intermediate App Dev Concepts is at the top of the stack
+    //There are 2 item(s) in the stack
+
+?>
